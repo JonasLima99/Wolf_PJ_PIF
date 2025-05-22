67ebe6efecb6ceb01a164a486bdf5c59b8f9718b
leolins77 <lfdln@cesar.school>
2025-05-22 09:32:37 -0300

    Modificando main para rodar o jogo, promovendo melhor modularização

--- src/main.c
+++ src/main.c
@@ -1,96 +1,31 @@
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h> 
-
-char campo_real[4][4];
-char visao_jogador[4][4];
-
-// Criei essa função para que seja adicionado um espaço em branco em cada linha e coluna da matriz.
-void inicializar_campo_real () {
-
-    for (int i=0; i < 4; i++) {
-        for(int j=0; j < 4; j++) {
-            campo_real[i][j] = ' ';
-        }
-    }
-}
-
-//Agora, precio criar uma função de teste, para confirmar se a matriz foi inicializada corretamente.
-
-void teste_inicializar_campo_real () {
-
-    for (int i=0; i < 4; i++) {
-        for(int j=0; j < 4; j++) {
-            printf("| %c ", campo_real[i][j]);;
-        }
-        printf("\n");
-        printf("--------------\n");
-    }
-}
-
-//Com a matriz inicializada, vou criar a próxima função para esconder alguns virus no campo de jogo.
-
-void sortear_virus () {
-
-    int qtd_virus = 4;
-    int contador = 0;
-
-    while (contador < qtd_virus) {
-
-        int linha = rand() % 4;
-        int coluna = rand() % 4;
-
-        if (campo_real[linha][coluna] == ' ') {
-            campo_real[linha][coluna] = 'V';
-            contador++;
-        }
-    }
-}
-
-//Agora, preciso criar o painel de visão do jogador, e ele não pode visualizar onde está o vírus...
-
-void inicializar_visao_jogador () {
-
-    for (int i=0; i < 4; i++) {
-        for(int j=0; j < 4; j++) {
-            visao_jogador[i][j] = 'x';
-        }
-    }
-}
-
-//Agora, precio criar uma função de teste, para confirmar se a visão do jogador foi inicializada corretamente, sem que ele visualize onde estão os vírus.
-
-void teste_visao_jogador () {
-
-    for (int i=0; i < 4; i++) {
-        for(int j=0; j < 4; j++) {
-            printf("| %c ", visao_jogador[i][j]);;
-        }
-        printf("\n");
-        printf("--------------\n");
-    }
-}
+#include <time.h>
+#include "game.h"
+#include "screen.h"
+#include "keyboard.h"
 
 int main() {
-   
-    inicializar_campo_real ();
-    teste_inicializar_campo_real ();
-
-    printf(" \n");
-    printf("--------------APOS INSERIR O VIRUS--------------\n");
-    printf(" \n");
+    char nome[50];
+    int num_virus;
 
     srand(time(NULL));
+    screenInit(1);
+    keyboardInit();
 
-    sortear_virus ();
-    teste_inicializar_campo_real(); 
+    screenSetColor(CYAN, BLACK);
+    printf("Digite seu nome: ");
+    scanf("%49s", nome);
 
-    printf(" \n");
-    printf("--------------VISAO JOGADOR--------------\n");
-    printf(" \n");
+    do {
+        printf("\nQuantidade de vírus (1 a 8): ");
+        scanf("%d", &num_virus);
+    } while (num_virus < 1 || num_virus > 8);
 
-    inicializar_visao_jogador ();
-    teste_visao_jogador ();
+    startGame(nome, num_virus);
 
+    keyboardDestroy();
+    screenDestroy();
     return 0;
 }
